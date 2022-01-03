06d638c2e5f34af5d020b93d02d611b0ee19b039
5KRC1 <skrjancmatic14@gmail.com>
2022-01-03 16:25:18 +0100

    fixed file check and clean code

--- .buildozer/android/app/main.py
+++ .buildozer/android/app/main.py
@@ -1,4 +1,4 @@
-#===clean code and make comments===
+# ===clean code and make comments===
 
 # ====other imports====
 import subprocess
@@ -10,7 +10,7 @@ import plyer
 # ====kivymd imports====
 from kivymd.app import MDApp
 from kivymd.uix.gridlayout import MDGridLayout
-from kivymd.uix.list import OneLineIconListItem, TwoLineAvatarIconListItem
+from kivymd.uix.list import TwoLineIconListItem, TwoLineAvatarIconListItem
 
 # ====kivy imports====
 from kivy import platform
@@ -24,20 +24,22 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 
 # ===global variables===
-LinuxPath = 'watchTube/'
 AndroidPath = '/storage/emulated/0/Download/watchTube/'
-WindowsPath = 'watchTube/'
+specialCharacters = '"\'@#$%^&*`~.'
 
 
 # ===send notifications===
 def sendNotification(title, message):
+    # toast notification, because normal not working with plyer :(
     plyer.notification.notify(title=title, message=message, toast=True)
 
 
 # ===title generator===
 def createTitle(title):
-    title = title.replace('"', '').replace(
-        "'", '').replace('?', '').replace(',', '').replace('.', '').replace(' ', '_')
+    # removes special characters in title
+    for char in specialCharacters:
+        title = title.replace(char, '')
+    title = title.replace(" ", "_")
     return title
 
 
@@ -47,13 +49,17 @@ def checkDir(path):
     return files
 
 
-# ===check if file exists
+#not working
+# ===check if file already exists===
 def checkFiles(path, title, content):
     files = os.listdir(path)
-    form = 'mp4' if content else 'mp3'
+    form = 'mp4' if not content else 'mp3'
+    title = createTitle(title)
+
     for file in files:
-        if file.split('.')[0] == createTitle(title):
-            if file.split('.')[:-1] == form:
+        fileInfo = file.split('.')
+        if fileInfo[0] == title:
+            if fileInfo[-1] == form:
                 return False
     return True
 
@@ -70,12 +76,17 @@ def startDownloadAudio(audio, directory):
         f'{directory}', filename=createTitle(audio.title) + '.mp3'))
 
 
+# ===Design of files displayed===
 class LWI(TwoLineAvatarIconListItem):
-    iconImage = StringProperty("")
+    # icon is, for some reason, also added ontop of text
+    iconImage = StringProperty('android')
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        # Sets icon for music and video
         self.iconImage = "movie-outline" if self.text == "Video" else "music"
 
+    # delete file
     def delete(self, a):
         typ = a.text
         title = a.secondary_text
@@ -85,7 +96,7 @@ class LWI(TwoLineAvatarIconListItem):
         sendNotification("Success", "File deleted")
 
 
-
+# ===main display===
 class Kv(MDGridLayout):
     url = ObjectProperty(None)  # gets url from input
     contentType = BooleanProperty(False)  # False == video, True == audio
@@ -114,75 +125,32 @@ class Kv(MDGridLayout):
                         self.addWidget(createTitle(yt.title) + ".mp3")
                         startDownloadAudio(yt, AndroidPath)
                 else:
-                    sendNotification('Already Downloaded', 'File already exists, look for it in the stack below!')
-
-            # # for windows
-            # elif platform == 'windows':
-            #     if checkFiles(WindowsPath, yt.title, self.contentType):
-            #         if self.contentType == False:
-            #             startDownloadVideo(yt, WindowsPath)
-            #         else:
-            #             startDownloadAudio(yt, WindowsPath)
-            #     else:
-            #         sendNotification(
-            #             'Already Downloaded', 'File already exists, look for it in the stack below!')
-
-            # # for linux
-            # elif platform == 'linux':
-            #     if checkFiles(LinuxPath, yt.title, self.contentType):
-            #         if self.contentType == False:
-            #             startDownloadVideo(yt, LinuxPath)
-            #         else:
-            #             startDownloadAudio(yt, LinuxPath)
-
-            #     else:
-            #         sendNotification(
-            #             'Already Downloaded', 'File already exists, look for it in the stack below!')
+                    sendNotification(
+                        'Already Downloaded', 'File already exists!')
 
         except Exception as e:
             print('\n===============error===============\n')
             print(e)
             print('\n===============error===============\n')
 
-
-    # gets downloaded videos and audios
+    # Gets downloaded videos and audios
     def getDownloads(self):
         if platform == 'android':
             if checkDir(AndroidPath):
                 for i in checkDir(AndroidPath):
-                    self.ids.list.add_widget(LWI(text='Video' if str(i).split(".")[1] == "mp4" else "Music", secondary_text=str(i).replace(".mp4", '').replace(".mp3", ''), on_press=self.openFile))
-                    # self.ids.list.add_widget(OneLineIconListItem(text='helo'))
+                    self.ids.list.add_widget(LWI(text='Video' if str(i).split(".")[1] == "mp4" else "Music", secondary_text=str(
+                        i).replace(".mp4", '').replace(".mp3", ''), on_press=self.openFile))
 
             else:
                 sendNotification('Empty', 'No videos yet downloaded!')
 
-
-
-        # if platform == 'windows':
-        #     for child in self.ids.list.children:
-        #         self.ids.list.remove_widget(child)
-        #     if checkDir(WindowsPath):
-        #         for i in checkDir(WindowsPath):
-        #             self.ids.list.add_widget(OneLineListItem(
-        #                 text=f'{i}', on_release=self.openFile))
-        #     else:
-        #         sendNotification('Empty', 'No videos yet downloaded!')
-
-        # if platform == 'linux':
-        #     for child in self.ids.list.children:
-        #         self.ids.list.remove_widget(child)
-        #     if checkDir(LinuxPath):
-        #         for i in checkDir(LinuxPath):
-        #             self.ids.list.add_widget(OneLineListItem(
-        #                 text=f'{i}', on_release=self.openFile))
-        #     else:
-        #         sendNotification('Empty', 'No videos yet downloaded!')
-
+    # Opens file on click
     def openFile(self, a):
-        title = a.secondary_text
         typ = a.text
+        title = a.secondary_text
         file = title + ".mp4" if typ == "Video" else title + ".mp3"
         if platform == 'android':
+
             # open in default app
             from jnius import autoclass, cast
             context = autoclass('org.kivy.android.PythonActivity').mActivity
@@ -200,24 +168,16 @@ class Kv(MDGridLayout):
 
                 context.startActivity(intent)
 
-        # if platform == 'windows':
-        #     path = WindowsPath + file
-        #     # startfile(path)
-
-        # if platform == 'linux':
-        #     path = LinuxPath + file
-        #     subprocess.call(['xdg-open', path])
-
-
+    # Adds widget
     def addWidget(self, text):
-        self.ids.list.add_widget(LWI(text='Video' if str(text).split(".")[1] == "mp4" else "Music", secondary_text=str(text).replace(".mp4", '').replace(".mp3", ''), on_press=self.openFile))
-
+        self.ids.list.add_widget(LWI(text="Video" if str(text).split(".")[-1] == "mp4" else "Music", secondary_text=str(
+            text).replace(".mp4", '').replace(".mp3", ''), on_press=self.openFile))
 
 
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = 'Dark'
-        self.theme_cls.primary_palette = 'BlueGray'
+        self.theme_cls.primary_palette = 'Red'
 
         kv = Kv()
         kv.getDownloads()
@@ -226,7 +186,8 @@ class MainApp(MDApp):
         if platform == 'android':
             # requests permissions for android storage to download to folder
             from android.permissions import request_permissions, Permission
-            request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
+            request_permissions(
+                [Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
 
         return kv
 
