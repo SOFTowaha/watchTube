2643bdeb03048761bef29efaade0f30d4560277e
5KRC1 <skrjancmatic14@gmail.com>
2022-12-11 11:06:18 +0100

    removed unwanted

--- .buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/enc/config_enc.o.d
+++
@@ -1,8 +0,0 @@
-/home/skrci/Projects/Python/Kivy/ytDownloader/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/enc/config_enc.o: \
-  /home/skrci/Projects/Python/Kivy/ytDownloader/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/enc/config_enc.c \
-  /home/skrci/Projects/Python/Kivy/ytDownloader/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/webp/encode.h \
-  /home/skrci/Projects/Python/Kivy/ytDownloader/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/webp/./types.h
-
-/home/skrci/Projects/Python/Kivy/ytDownloader/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/webp/encode.h:
-
-/home/skrci/Projects/Python/Kivy/ytDownloader/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/webp/./types.h:
